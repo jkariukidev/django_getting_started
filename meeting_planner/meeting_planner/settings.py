bc211346e761b578f80f826fbfb1dc7fdef9dbca
Joseph Kariuki <joehene@gmail.com>
2020-12-15 13:25:08 +0300

    s

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '20#gz1%@d2n46b6dp$*)u#c0m^oz3$$^uo65()==i=ua85o9p_'
+# SECRET_KEY = '20#gz1%@d2n46b6dp$*)u#c0m^oz3$$^uo65()==i=ua85o9p_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
